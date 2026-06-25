43658be235be7ba7a94f5439200a7a57c29e7dfa
MarinaJim <sacharovamarina@gmail.com>
2026-06-25 20:22:55 +0200

    continue showcase

--- static/js/aitd.js
+++ static/js/aitd.js
@@ -15,24 +15,28 @@
     notions.forEach(notion => {
         $.getJSON("notions/" + notion + ".json", function (data) {
             const el = document.getElementById(notion);
-            el.addEventListener("click", () => renderSegments(data));
+            el.addEventListener("click", () => renderSegments(data, notion));
             notionsData.push({ el, data });
         });
     });
 
-    function renderSegments(segments) {
-        console.log("Segments: ", segments);
-        const html = segments.map(seg => {
+    function renderSegments(segments, notion) {
+        const html = segments.map((seg, i) => {
             const cls = seg.author === "User"? "aitd-seg-user" : "aitd-seg-bot";
-            return '<span class="aitd-seg ' + cls + '">' + escapeHtml(seg.text)+'</span>';
+            const nextText = i < segments.length - 1 ? segments[i + 1].text : null;
+            return '<span class="aitd-seg ' + cls + '">' + constructText(seg.text, notion, nextText) + '</span>';
         }).join("");
         textBody.innerHTML = html;
     }
 
-    function escapeHtml(html) {
-        return String(html)
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;");
+    function constructText(data, notion, nextText) {
+        if (["documentLevel", "spanLevel", "boundaryLevel"].includes(notion)) return data;
+        if (notion === "membershipLevel") { 
+            if (data == "(" || [")", ",", "."].includes(nextText)) return data;
+            if (nextText === null) return data + ".";
+            return data + " ";
+        }
+        return data + " ";
     }
+
 })();
